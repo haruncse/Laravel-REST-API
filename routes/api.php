00705ea01e309e0cd45e4627293c7ea455aa1c1d
haruncse <harunce12008@gmail.com>
2020-09-19 17:59:43 +0600

    JWT Auth Added

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,17 @@
 
 Route::apiResource('/laravel-api','Api\ApiTestController');
 Route::apiResource('/customer-api','CustomerInfoController');
+
+
+Route::group([
+
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
